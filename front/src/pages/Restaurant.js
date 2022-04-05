c35a93a764d1894b8c47cf4bff097bce5140b6ad
bastien-cordier <bastien.cordier78@gmail.com>
2022-04-05 12:41:49 +0200

    fix conflict

--- front/src/pages/Restaurant.js
+++ front/src/pages/Restaurant.js
@@ -1,18 +1,41 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./Restaurant.scss";
 
 function Restaurant() {
   return (
     <Fragment>
-    <img src="/assets/interieur.png" alt="interieur" className='notre-restaurant' />
-    <h1>NOTRE RESTAURANT</h1>
-    <p>
-      Au coeur du 3ème arrondissement de Paris, Pizza de la mama de napolita vous offre une expérience culinaire comme nulle part ailleurs. Vous y retrouverez une cuisine de qualité rassemblant toutes les saveurs délicieuses de l'Italie.
-      Réalisées par nos chefs avec amour et des produits frais de qualité, venez tester nos best-sellers qui raviront vos papilles.
-      Faites-vous livrer ou vivez l'expérience sur place ou à emporter. A bientôt chez Pizza de la mama 🍕
-    </p>
+      <div className="restaurant">
+        <img src="/assets/interieur.png" alt="interieur" className="img-bandeau" />
+        <Container>
+          <Row>
+            <Col sm={6}>
+              <h1>NOTRE RESTAURANT</h1>
+              <p>
+                <span style={{ fontSize: "18px" }}>🧑🏼‍🍳</span> Au coeur du 3ème arrondissement de
+                Paris, Pizza de la mama de napolita vous offre une expérience culinaire comme nulle
+                part ailleurs. Vous y retrouverez une cuisine de qualité rassemblant toutes les
+                saveurs délicieuses de l'Italie.
+              </p>
+              <p>
+                <span style={{ fontSize: "18px" }}>🍕</span> Réalisées par nos chefs avec amour et
+                des produits frais de qualité, venez tester nos best-sellers qui raviront vos
+                papilles.
+              </p>
+              <p>
+                <span style={{ fontSize: "18px" }}>👋🏼</span> Faites-vous livrer ou vivez
+                l'expérience sur place ou à emporter. A bientôt chez Pizza de la mama de Napolita
+              </p>
+            </Col>
+            <Col sm={1}></Col>
+            <Col sm={5}>
+              <img src="assets/about.jpeg" alt="Notre Restaurant" />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </Fragment>
-  )
+  );
 }
 
-export default Restaurant
+export default Restaurant;
