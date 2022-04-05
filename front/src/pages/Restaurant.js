5053bcc4996cb9c0d9ec61edb39bdc30926b7a37
Hugo Fief <hfief1806@gmail.com>
2022-04-05 12:08:55 +0200

    update restaurant component

--- front/src/pages/Restaurant.js
+++ front/src/pages/Restaurant.js
@@ -6,13 +6,11 @@ function Restaurant() {
     <Fragment>
     <img src="/assets/interieur.png" alt="interieur" className='notre-restaurant' />
     <h1>NOTRE RESTAURANT</h1>
-    <p>Au coeur du 3ème arrondissement de Paris, Pizza de la mama de napolita vous offre une expérience culinaire comme nulle part ailleurs. Vous y retrouverez une cuisine de qualité rassemblant toutes les saveurs délicieuses de l'Italie.
-
-Réalisées par nos chefs avec amour et des produits frais de qualité, venez tester nos best-sellers qui raviront vos papilles.
-
-Faites-vous livrer ou vivez l'expérience sur place ou à emporter.
-
-A bientôt chez Pizza de la mama 🍕</p>
+    <p>
+      Au coeur du 3ème arrondissement de Paris, Pizza de la mama de napolita vous offre une expérience culinaire comme nulle part ailleurs. Vous y retrouverez une cuisine de qualité rassemblant toutes les saveurs délicieuses de l'Italie.
+      Réalisées par nos chefs avec amour et des produits frais de qualité, venez tester nos best-sellers qui raviront vos papilles.
+      Faites-vous livrer ou vivez l'expérience sur place ou à emporter. A bientôt chez Pizza de la mama 🍕
+    </p>
     </Fragment>
   )
 }
