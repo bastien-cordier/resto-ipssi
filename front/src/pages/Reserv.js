ccef2018ca9d0a312d6eb764f7bc7e4218ddbb2b
Jordan BRUNEEL <j.bruneel@ecole-ipssi.net>
2022-04-06 16:38:36 +0200

    page reservation

--- front/src/pages/Reserv.js
+++ front/src/pages/Reserv.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Card from "components/Cards/Card";
+import "./Reserv.scss";
+
 
 const Reserv = () => {
-  return <div>Reserv</div>;
-};
+  return (
+    <div>
+      <img
+        src="/assets/banner-reserv.png"
+        alt="banner"
+        className="img-bandeau"
+        style={{ width:"100%" }}
+      />
+      <Container className="test">
+        <h3> 🍽 Reservez votre table</h3>
+      </Container>
+      <Container className="pizzas">
+        <Card />
+        <Card />
+        <Card />
+      </Container>
 
+    </div>
+  );
+};
 export default Reserv;
