043ba8503f21e9efbc9efbce5a66ea9e9950df9e
Hugo Fief <hfief1806@gmail.com>
2022-04-06 12:42:38 +0200

    edit spaces

--- front/src/App.js
+++ front/src/App.js
@@ -42,7 +42,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home/>} />
 
           <Route path="/notre-restaurant" element={<Restaurant/>} />
           <Route path="/commander" element={<Commander/>} />
