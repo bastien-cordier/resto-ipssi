14406f45b7da1da127a245badfa1bfcf787e59a1
Markcley <markcleymic@gmail.com>
2022-04-06 12:54:45 +0200

    cors

--- back/public/index.php
+++ back/public/index.php
@@ -2,6 +2,21 @@
 
 use Core\Routeur\Routeur;
 
+$allowedOrigins = array(
+    "http://localhost:3000"
+);
+
+if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
+    foreach ($allowedOrigins as $allowedOrigin) {
+        if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
+        header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+        header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+        header('Access-Control-Max-Age: 1000');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+        break;
+        }
+    }
+}
 define("ROOT", dirname(__DIR__));
 require_once ROOT . "/vendor/autoload.php";
 Routeur::Routes();
