a9bf14fe81d7675016e669b1cc4c63ed0a9ece8f
Markcley <markcleymic@gmail.com>
2022-04-06 15:33:50 +0200

    Cors

--- back/public/index.php
+++ back/public/index.php
@@ -5,7 +5,7 @@ use Core\Routeur\Routeur;
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Allow-Headers: token, Content-Type, Authorization');
     header('Access-Control-Max-Age: 1728000');
     header('Content-Length: 0');
     header('Content-Type: text/plain');
@@ -14,10 +14,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization,  X-Requested-With');
-header("Access-Control-Allow-Credentials: true");
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-header('Access-Control-Max-Age: 1000');
 
 
 define("ROOT", dirname(__DIR__));
