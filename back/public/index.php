588663070a6d1aafc1910d5d64a25b3c8cbdcc07
Markcley <markcleymic@gmail.com>
2022-04-06 14:42:53 +0200

    CORS

--- back/public/index.php
+++ back/public/index.php
@@ -2,21 +2,20 @@
 
 use Core\Routeur\Routeur;
 
-$allowedOrigins = array(
-    "http://localhost:3000"
-);
-
-if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
-    foreach ($allowedOrigins as $allowedOrigin) {
-        if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
-        header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-        header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-        header('Access-Control-Max-Age: 1000');
-        header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-        break;
-        }
-    }
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
 }
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+
 define("ROOT", dirname(__DIR__));
 require_once ROOT . "/vendor/autoload.php";
 Routeur::Routes();
