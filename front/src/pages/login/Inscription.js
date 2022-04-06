40c55c2f6b876f905048bb18b9d1a240ff419615
Hugo Fief <hfief1806@gmail.com>
2022-04-06 16:07:15 +0200

    fix bug inscription

--- front/src/pages/login/Inscription.js
+++ front/src/pages/login/Inscription.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
 import axios from "axios";
-import { ApiRequests,, Header, ErrorInfosMessage } from "api/BaseApi";
+import { ApiRequests, Header, ErrorInfosMessage } from "api/BaseApi";
 import Swal from "sweetalert2";
 import "./Log.scss";
 
